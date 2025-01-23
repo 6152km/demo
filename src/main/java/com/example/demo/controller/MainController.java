465a6156e2a1998d64dbbfdd7d2c151bc4e24587
6152km <kmg6152@gmail.com>
2025-01-23 17:41:44 +0900

    third commit

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-	@GetMapping("/")
+	@GetMapping({"/", ""})
 	public String main() {
 		
-		return"";
+		return"index";
 	}
 }
